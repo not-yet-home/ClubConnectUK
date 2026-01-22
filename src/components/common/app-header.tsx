7fff6ac873eca89fb0fbc4346812525136111196
Yet Joshua <rockbed009@gmail.com>
2026-01-22 19:56:50 +0800

    ✨ Enhance broadcast page

--- src/components/common/app-header.tsx
+++ src/components/common/app-header.tsx
@@ -17,7 +17,7 @@ interface AppHeaderProps {
 
 export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
     return (
-        <header className="flex h-14 shrink-0 items-center gap-2 bg-background rounded-tl-md rounded-tr-md border-b border-border px-4">            <SidebarTrigger />
+        <header className="flex sticky top-0 z-50 h-14 shrink-0 items-center gap-2 bg-background rounded-tl-md rounded-tr-md border-b border-border px-4">            <SidebarTrigger />
             <Breadcrumb>
                 <BreadcrumbList>
                     {breadcrumbs.map((crumb, index) => (
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -311,7 +311,7 @@ function SidebarInset({ className, ...props }: React.ComponentProps<"main">) {
     <main
       data-slot="sidebar-inset"
       className={cn(
-        "relative rounded-md flex w-full h-full border border-border flex-1 flex-col",
+        "relative rounded-md flex w-full max-h-[calc(100vh-24px)] border border-border flex-1 flex-col",
         "md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-2",
         className
       )}
